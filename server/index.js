76b83502288119730a56ac2e49c8e15e55c8e0d1
Lakshya SIngh <iD024>
2025-09-11 00:53:10 +0530

    added routes

--- server/index.js
+++ server/index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const connectionDB = require("./config/db");
 const UserSchema = require("./models/user");
 
+//creation of server
 const app = express();
 const PORT = 5000;
 
+//middlewares :
+app.use(express.json({ extended: false })); //
+app.use("/api/users", require("./routes/users")); //
+
+//main route aka main page
+app.get("/", (req, res) => {
+  res.send("Connect to CloudPi");
+});
+
+// makes the server listen
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });

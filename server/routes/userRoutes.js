ae66dd87e78d803305948a71b35b0b49f92a15cf
Lakshya SIngh <iD024>
2025-09-11 01:25:50 +0530

    added login + updated the routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const registerUser = require("../controllers/userController");
 
-router.post("/register", (req, res) => {
-  res.send("register user route");
-});
+router.post("/register", registerUser);
 
 module.exports = router;

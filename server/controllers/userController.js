75989c58cbc81853151efa06c63aa7721e04873e
Lakshya SIngh <iD024>
2025-09-11 10:03:35 +0530

    added login feature + updated the route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -49,3 +49,35 @@ exports.registerUser = async (req, res) => {
     console.error(err.message);
   }
 };
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ msg: "Account doesn't Exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(payload, "TokenKEY", { expiresIn: 3600 }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+module.exports = { registerUser, loginUser };
